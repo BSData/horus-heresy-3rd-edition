bf5def4083ce2a23437e60cea0d08a0f3f7c9c3f
Noah Stephen Haskell <nstephenh@gmail.com>
2025-08-04 17:33:15 -0500

    Test with submodule

---
+++ tests/validatehh3.py
@@ -0,0 +1,25 @@
+import sys
+
+# Temp until we have a pip module
+sys.path.insert(1, "BSCopy")
+
+from BSCopy.system.system import System
+
+
+
+if __name__ == '__main__':
+
+    system = System('horus-heresy-3rd-edition',
+                    settings={
+                        # SystemSettingsKeys.GAME_IMPORT_SPEC: GameImportSpecs.HERESY3E,
+                    },
+                    )
+    for file in system.files:
+        entry_links_node = file.root_node.get_child(tag='entryLinks')
+        if entry_links_node is None:
+            continue
+        for child in entry_links_node.children:
+            category_count = len(child.get_categories())
+            if category_count != 1:
+                print(f"{child} has {category_count} categories, only 1 is expected")
+
