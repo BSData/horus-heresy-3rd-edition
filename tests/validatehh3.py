5daabeb17a5ca23c5319a1e5c627734c9ab28e9e
Noah Stephen Haskell <nstephenh@gmail.com>
2025-09-02 18:49:01 -0500

    All forces should be linked now that there's a delagatus

--- tests/validatehh3.py
+++ tests/validatehh3.py
@@ -223,6 +223,13 @@ class GameTests(unittest.TestCase):
         allied_links = self.system.get_node_by_id("256b-b8a8-017a-75e9").get_child("forceEntryLinks")
         for child_force in crusade.get_child("forceEntries").children:
             # print("\t", child_force)
+            with self.subTest(f"{child_force.name} should be linked in the Allied Detachment"):
+                self.assertIsNotNone(allied_links.get_child("forceEntryLink", attrib={"targetId": child_force.id}))
+
+    def test_all_max_0_detachments_have_a_hide(self):
+        crusade = self.system.get_node_by_id("8562-592c-8d4b-a1f0")
+        for child_force in crusade.get_child("forceEntries").children:
+            # print("\t", child_force)
             if not child_force.name.startswith("Auxiliary - "):
                 continue
             with self.subTest(f"{child_force.name} should be linked in the Allied Detachment"):
